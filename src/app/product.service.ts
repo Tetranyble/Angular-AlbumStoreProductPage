d92907da0c91653d38007e9aebbf5b57116d7cd2
Leonard Ugbanawaji Ekenekiso <senenerst@gmail.com>
2020-10-10 22:50:38 +0100

    exercise three completed

---
+++ src/app/product.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+
+@Injectable()
+export class ProductService {
+  private _albumUrl: string = '../assets/album.json'
+  constructor(private _http: Http) { }
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl).map(response => response.json())
+  }
+
+  private handleError (err: Response){
+
+  }
+}
